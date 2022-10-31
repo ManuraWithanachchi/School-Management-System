638dad793fce2e1f4fe5c3c6b8ef75a3d4f51b62
Manura Withanachchi <manurawithanachchi@gmail.com>
2022-10-31 23:48:34 +0530

    Update the project

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
+import "./App.css";
+
+// Notifications
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// Components
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Footer from "./Components/Footer/Footer";
+import Intro from "./Components/Intro/Intro";
+import StudentPage from "./Components/StudentPage/StudentPage";
+import LecturerPage from "./Components/LecturerPage/LecturerPage";
+import LecturerAdding from "./Components/LecturerPage/LecturerAdding";
+import Department from "./Components/LecturerPage/Department";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <ToastContainer position="top-center" />
+        <div>
+          <Routes>
+            <Route exact path="/studentPage" element={<StudentPage />} />
+            <Route exact path="/" element={<Intro />} />
+            <Route exact path="/lecturerPage" element={<LecturerPage />} />
+            <Route
+              exact
+              path="/lecturer/addLecturer/"
+              element={<LecturerAdding />}
+            />
+            <Route
+              exact
+              path="lecturer/update/:id"
+              element={<LecturerAdding />}
+            />
+            <Route exact path="lecturer/department" element={<Department />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
